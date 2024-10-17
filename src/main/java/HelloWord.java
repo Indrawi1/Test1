0ed4d5f5af172adb2afdd88e6df3a28b059ca4e6
Milena Runes <milena.runets@gmail.com>
2024-10-17 18:30:22 +0200

    XD4
    XD3

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -3,7 +3,12 @@ import java.util.stream.Stream;
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!222");
+        System.out.println("Hello, World!111");
+        System.out.println("Hello, World!321");
+        System.out.println("Hello, World!123");
+        System.out.println("Hello, World!123");
+        System.out.println("Hello, World!123");
 
     }
 }
