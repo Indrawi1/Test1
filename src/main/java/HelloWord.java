6ed0bc6ffbb0339586aafb09f7be6edf4fc7d825
Milena Runes <milena.runets@gmail.com>
2024-10-17 18:22:49 +0200

    git init

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+}
